350bf2319386737fbe9bc20583db10d882ff4795
anonymoussc <50c5ac69@opayq.com>
2017-10-22 03:08:12 +0700

    New User Models

---
+++ src/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Components\Passerby\Models;
+
+use App\User as AppUser;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class User extends AppUser
+{
+    use HasApiTokens, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}

83cc63d34af72a8cb18de6bde9ce66fbba77417a
anonymoussc <50c5ac69@opayq.com>
2017-10-22 03:07:07 +0700

    New Controller Http\Controllers

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Controller.php
+ * Created by rn on 10/22/2017 1:26 AM.
+ */
+
+namespace App\Components\Passerby\Http\Controllers;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Optimus\Bruno\LaravelController;
+
+abstract class Controller extends LaravelController
+{
+
+}

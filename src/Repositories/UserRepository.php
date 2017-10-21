80eccf76cd4d292006efc97e6c8e852e72fc245b
anonymoussc <50c5ac69@opayq.com>
2017-10-22 03:09:06 +0700

    New UserRepository Repositories

---
+++ src/Repositories/UserRepository.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * UserRepository.php
+ * Created by rn on 10/22/2017 1:31 AM.
+ */
+
+namespace App\Components\Passerby\Repositories;
+
+use App\Components\Passerby\Models\User;
+use App\Components\Passerby\Repositories\DatabaseRepository;
+
+class UserRepository extends DatabaseRepository
+{
+    public function getModel()
+    {
+        return new User();
+    }
+
+    public function create(array $data)
+    {
+        $user = $this->getModel();
+
+        $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+
+        $user->fill($data);
+        $user->save();
+
+        return $user;
+    }
+
+    public function update(User $user, array $data)
+    {
+        $user->fill($data);
+
+        $user->save();
+
+        return $user;
+    }
+}

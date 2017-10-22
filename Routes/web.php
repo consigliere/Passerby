8f59a2f7c5b63231368f4cbb5d489db4bf6f439a
anonymoussc <50c5ac69@opayq.com>
2017-10-22 07:54:33 +0700

    Modified web Routes

--- Routes/web.php
+++ Routes/web.php
@@ -4,6 +4,8 @@
  * Created by rn on 10/22/2017 1:23 AM.
  */
 
-//Route::group(['middleware' => 'web', 'prefix' => 'passerby', 'namespace' => 'App\\Components\Passerby\Http\Controllers'], function() {
-//    Route::get('/', 'PasserbyController@index');
-//});
+if (config('route.web')) {
+    Route::group(['middleware' => 'web', 'prefix' => 'passerby', 'namespace' => 'App\\Components\Passerby\Http\Controllers'], function () {
+        Route::get('/', 'PasserbyController@index');
+    });
+}

d248715b43730908145411177ee4c60178919326
anonymoussc <50c5ac69@opayq.com>
2017-10-22 07:54:21 +0700

    Modified api Routes

--- Routes/api.php
+++ Routes/api.php
@@ -3,19 +3,18 @@
  * api.php
  * Created by rn on 10/22/2017 1:22 AM.
  */
-Route::group(['middleware' => 'api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
-    Route::post('/login', 'LoginController@login');
-    Route::post('/login/refresh', 'LoginController@refresh');
-});
 
-Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
-    Route::post('/logout', 'LoginController@logout');
-});
+if (config('passerby.route.api')) {
+    Route::group(['middleware' => 'api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
+        Route::post('/login', 'LoginController@login');
+        Route::post('/login/refresh', 'LoginController@refresh');
+    });
 
-Route::group(['middleware' => 'api', 'prefix' => 'passerby', 'namespace' => 'App\Components\Passerby\Http\Controllers'], function () {
-    //
-});
+    Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
+        Route::post('/logout', 'LoginController@logout');
+    });
 
-//Route::group(['middleware' => 'api', 'prefix' => 'passerby', 'namespace' => 'App\\Components\Passerby\Http\Controllers'], function() {
-//    //
-//});
+    Route::group(['middleware' => 'api', 'prefix' => 'passerby', 'namespace' => 'App\Components\Passerby\Http\Controllers'], function () {
+        //
+    });
+}

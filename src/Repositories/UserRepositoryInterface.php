e5440b12c172ccb783ff445300e474e4bf01e3af
anonymoussc <50c5ac69@opayq.com>
2017-10-23 08:39:34 +0700

    New UserRepositoryInterface Repositories

---
+++ src/Repositories/UserRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * UserRepositoryInterface.php
+ * Created by rn on 10/23/2017 12:58 AM.
+ */
+
+namespace App\Components\Passerby\Repositories;
+
+
+interface UserRepositoryInterface
+{
+
+}

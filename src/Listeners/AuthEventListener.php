a0bfabe0a1a3bcf05e595911d2ebbdb5ae84312c
anonymoussc <50c5ac69@opayq.com>
2017-10-24 15:57:36 +0700

    Modified AuthEventListener Listeners

--- src/Listeners/AuthEventListener.php
+++ src/Listeners/AuthEventListener.php
@@ -9,5 +9,37 @@ namespace App\Components\Passerby\Listeners;
 
 class AuthEventListener
 {
+    public function subscribe($events)
+    {
+        $events->listen('user.login', 'App\Components\Passerby\Listeners\AuthEventListener@onUserLogin', 10);
+        $events->listen('user.logout', 'App\Components\Passerby\Listeners\AuthEventListener@onUserLogout', 10);
+        $events->listen('user.register', 'App\Components\Passerby\Listeners\AuthEventListener@onUserRegister', 10);
+        $events->listen('user.resend', 'App\Components\Passerby\Listeners\AuthEventListener@resend', 10);
+        $events->listen('user.reset', 'App\Components\Passerby\Listeners\AuthEventListener@passwordReset', 10);
+    }
 
+    public function onUserLogin()
+    {
+
+    }
+
+    public function onUserLogout()
+    {
+
+    }
+
+    public function onUserRegister()
+    {
+
+    }
+
+    public function resend()
+    {
+
+    }
+
+    public function passwordReset()
+    {
+
+    }
 }

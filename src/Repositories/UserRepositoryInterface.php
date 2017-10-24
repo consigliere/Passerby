c349704b14db13d1f780bbde73369aca2ae08d4a
anonymoussc <50c5ac69@opayq.com>
2017-10-24 15:59:12 +0700

    Modified UserRepositoryInterface Repositories

--- src/Repositories/UserRepositoryInterface.php
+++ src/Repositories/UserRepositoryInterface.php
@@ -6,8 +6,11 @@
 
 namespace App\Components\Passerby\Repositories;
 
+use App\Components\Passerby\Models\User;
 
 interface UserRepositoryInterface
 {
+    public function create(array $data);
 
+    public function update(User $user, array $data);
 }

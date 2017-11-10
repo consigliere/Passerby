b19ef649f0382d63cd9e09bc4730e83e519bd499
anonymoussc <50c5ac69@opayq.com>
2017-11-10 12:58:00 +0700

    New LoginRepository \Repositories

---
+++ src/Repositories/Login/LoginRepository.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * LoginRepository.php
+ * Created by rn on 11/10/2017 10:35 AM.
+ */
+
+namespace App\Components\Passerby\Repositories\Login;
+
+use App\Components\Passerby\Models\User;
+use App\Components\Passerby\Repositories\Repository;
+use App\Components\Passerby\Repositories\LoginRepositoryInterface;
+
+class LoginRepository extends Repository implements LoginRepositoryInterface
+{
+    private $userCfg;
+
+    public function getModel()
+    {
+        $this->userCfg = config('auth.providers.users.model');
+
+        return new $this->userCfg;
+    }
+
+    public function create(array $data)
+    {
+        $user = $this->getModel();
+
+        $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+
+        $user->fill($data);
+        $user->save();
+
+        return $user;
+    }
+
+    public function update(User $user, array $data)
+    {
+        $user->fill($data);
+
+        $user->save();
+
+        return $user;
+    }
+}

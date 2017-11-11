bba3aae979be34be22945fb1b3f827c944d61b9a
anonymoussc <50c5ac69@opayq.com>
2017-11-11 08:22:00 +0700

    New PermissionService App\Components\Passerby\Services

---
+++ src/Services/RoleService.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * RoleService.php
+ * Created by rn on 11/11/2017 6:18 AM.
+ */
+
+namespace App\Components\Passerby\Services;
+
+use Illuminate\Foundation\Application;
+use App\Components\Passerby\Exceptions\InvalidCredentialsException;
+use App\Components\Passerby\Repositories\RoleRepositoryInterface;
+
+class RoleService
+{
+    public function __construct(Application $app, RoleRepositoryInterface $roleRepository)
+    {
+        $this->roleRepository = $roleRepository;
+
+        $this->apiConsumer = $app->make('apiconsumer');
+        $this->auth        = $app->make('auth');
+        $this->cookie      = $app->make('cookie');
+        $this->db          = $app->make('db');
+        $this->request     = $app->make('request');
+    }
+}

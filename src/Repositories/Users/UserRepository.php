121646efecb38496c44a36c918f0f9fade1971b8
anonymoussc <50c5ac69@opayq.com>
2017-11-11 08:21:05 +0700

    New UserRepository App\Components\Passerby\Repositories\Users

---
+++ src/Repositories/Users/UserRepository.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * UserRepository.php
+ * Created by rn on 11/11/2017 5:34 AM.
+ */
+
+namespace App\Components\Passerby\Repositories\Users;
+
+use App\Components\Passerby\Models\User;
+use App\Components\Passerby\Repositories\Repository;
+use App\Components\Passerby\Repositories\UserRepositoryInterface;
+
+class UserRepository extends Repository implements UserRepositoryInterface
+{
+    private $userCfg;
+
+    public function getModel()
+    {
+        $this->userCfg = config('auth.providers.users.model');
+
+        return new $this->userCfg;
+    }
+}

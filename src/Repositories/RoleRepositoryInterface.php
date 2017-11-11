349ff25f1c210e071c24489df57f07236e143c54
anonymoussc <50c5ac69@opayq.com>
2017-11-11 08:19:28 +0700

    New RoleRepositoryInterface App\Components\Passerby\Repositories

---
+++ src/Repositories/RoleRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * RoleRepositoryInterface.php
+ * Created by rn on 11/11/2017 6:19 AM.
+ */
+
+namespace App\Components\Passerby\Repositories;
+
+
+interface RoleRepositoryInterface
+{
+
+}

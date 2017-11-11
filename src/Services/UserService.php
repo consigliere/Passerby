48cc0949a5930cad3518a95469157f9a9fe8abf0
anonymoussc <50c5ac69@opayq.com>
2017-11-11 08:22:22 +0700

    New UserService App\Components\Passerby\Services

---
+++ src/Services/UserService.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * UserService.php
+ * Created by rn on 11/11/2017 5:31 AM.
+ */
+
+namespace App\Components\Passerby\Services;
+
+use Illuminate\Foundation\Application;
+use App\Components\Passerby\Exceptions\InvalidCredentialsException;
+use App\Components\Passerby\Repositories\UserRepositoryInterface;
+
+class UserService
+{
+    public function __construct(Application $app, UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+
+        $this->apiConsumer = $app->make('apiconsumer');
+        $this->auth        = $app->make('auth');
+        $this->cookie      = $app->make('cookie');
+        $this->db          = $app->make('db');
+        $this->request     = $app->make('request');
+    }
+}

72559e39f6408c24ce8469a3a58f1094a3e7e60a
anonymoussc <50c5ac69@opayq.com>
2017-11-11 08:18:48 +0700

    New PermissionRepositoryInterface App\Components\Passerby\Repositories

---
+++ src/Repositories/PermissionRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PermissionRepositoryInterface.php
+ * Created by rn on 11/11/2017 6:20 AM.
+ */
+
+namespace App\Components\Passerby\Repositories;
+
+
+interface PermissionRepositoryInterface
+{
+
+}

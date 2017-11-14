54df2b4abb891e6ff63529656c4372a37c4465d4
anonymoussc <50c5ac69@opayq.com>
2017-11-14 18:06:21 +0700

    New PermissionCreateRequest  App\Components\Passerby\Requests

---
+++ src/Http/Requests/PermissionCreateRequest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PermissionCreateRequest.php
+ * Created by rn on 11/14/2017 4:26 PM.
+ */
+
+namespace App\Components\Passerby\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PermissionCreateRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:40',
+        ];
+    }
+}

dc6debe0cfca4babde9f44183d77a88989f6472e
anonymoussc <50c5ac69@opayq.com>
2017-11-14 18:07:41 +0700

    Modified PermissionController App\Components\Passerby\Http\Controllers

--- src/Http/Controllers/PermissionController.php
+++ src/Http/Controllers/PermissionController.php
@@ -6,8 +6,48 @@
 
 namespace App\Components\Passerby\Http\Controllers;
 
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Components\Passerby\Requests\PermissionCreateRequest;
+use App\Components\Passerby\Requests\PermissionUpdateRequest;
+use App\Components\Passerby\Services\PermissionService;
+
 
 class PermissionController
 {
+    private $permissionService;
+
+    public function __construct(PermissionService $permissionService)
+    {
+        $this->permissionService = $permissionService;
+    }
+
+    public function create(PermissionCreateRequest $request)
+    {
+        $name = $request->input('name');
+
+        $this->permissionService->create(['name' => $name]);
+
+        //$permission       = new Permission();
+        //$permission->name = $name;
+
+        //$permission->save();
+
+//        if (!empty($request['roles'])) { //If one or more role
+//            foreach ($roles as $role) {
+//                $r          = Role::where('id', '=', $role)->firstOrFail();
+//                $permission = Permission::where('name', '=', $name)->first();
+//                $r->givePermissionTo($permission);
+//            }
+//        }
+
+        //return redirect()->route('permissions.index')->with('flash_message', 'Permission' . $permission->name . ' added!');
+    }
+
+    public function update(PermissionUpdateRequest $request)
+    {
+        $id    = $request->input('id');
+        $value = $request->input('name');
+    }
 
 }

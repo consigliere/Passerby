b171e84268b22143016db6a4a371650c151dfaec
anonymoussc <50c5ac69@opayq.com>
2017-11-18 07:30:11 +0700

    Modified PermissionRepositoryInterface App\Components\Passerby\Repositories

--- src/Repositories/PermissionRepositoryInterface.php
+++ src/Repositories/PermissionRepositoryInterface.php
@@ -6,9 +6,6 @@
 
 namespace App\Components\Passerby\Repositories;
 
-use Spatie\Permission\Models\Permission;
-
-
 interface PermissionRepositoryInterface
 {
     public function create(array $data);

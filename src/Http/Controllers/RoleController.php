895910da9eca251ea1255852795a4e2f196a2a66
anonymoussc <50c5ac69@opayq.com>
2017-11-22 00:13:56 +0700

    Modified RoleController App\Components\Passerby\Http\Controllers

--- src/Http/Controllers/RoleController.php
+++ src/Http/Controllers/RoleController.php
@@ -6,8 +6,40 @@
 
 namespace App\Components\Passerby\Http\Controllers;
 
+use App\Components\Passerby\Services\RoleService;
+use App\Components\Passerby\Requests\RoleCreateRequest;
+use App\Components\Passerby\Requests\RoleUpdateRequest;
 
-class RoleController
+
+class RoleController extends Controller
 {
+    private $roleService;
 
+    public function __construct(RoleService $roleService)
+    {
+        $this->roleService = $roleService;
+    }
+
+    public function get()
+    {
+        $resourceOptions = $this->parseResourceOptions();
+
+        return $this->response($this->roleService->get($resourceOptions));
+
+    }
+
+    public function create(RoleCreateRequest $request)
+    {
+        return $this->response($this->roleService->create($request->all()));
+    }
+
+    public function update(RoleUpdateRequest $request, $id)
+    {
+        return $this->response($this->roleService->update($request->all(), $id));
+    }
+
+    public function delete($id)
+    {
+        return $this->response($this->roleService->delete($id));
+    }
 }

29004653d9cd22f7d1e7adf8f02ba14546fa218b
anonymoussc <50c5ac69@opayq.com>
2018-02-11 16:18:20 +0700

    Modified UserEventSubscriber App\Components\Passerby\Listeners

--- src/Listeners/UserEventSubscriber.php
+++ src/Listeners/UserEventSubscriber.php
@@ -1,11 +1,10 @@
 <?php
 /**
  * UserEventSubscriber.php
- * Created by rn on 11/11/2017 5:32 AM.
+ * Created by @anonymoussc on 11/11/2017 5:32 AM.
  */
 
 namespace App\Components\Passerby\Listeners;
-
 
 class UserEventSubscriber
 {

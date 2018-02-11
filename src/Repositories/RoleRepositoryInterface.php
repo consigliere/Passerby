d4102909fb0e69146d6c7eed512d24483a832a3e
anonymoussc <50c5ac69@opayq.com>
2018-02-11 16:23:00 +0700

    Modified RoleRepositoryInterface App\Components\Passerby\Repositories

--- src/Repositories/RoleRepositoryInterface.php
+++ src/Repositories/RoleRepositoryInterface.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * RoleRepositoryInterface.php
- * Created by rn on 11/11/2017 6:19 AM.
+ * Created by @anonymoussc on 11/11/2017 6:19 AM.
  */
 
 namespace App\Components\Passerby\Repositories;

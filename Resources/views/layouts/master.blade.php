56ca5e0c1590f82417e07ba72a36c53b866f80ab
anonymoussc <50c5ac69@opayq.com>
2018-11-20 17:18:58 +0700

    Update Resources

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -4,9 +4,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Component Passerby</title>
+        <title>Module Passerby</title>
+
+       {{-- Laravel Mix - CSS File --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/passerby.css') }}"> --}}
+
     </head>
     <body>
         @yield('content')
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/passerby.js') }}"></script> --}}
     </body>
 </html>

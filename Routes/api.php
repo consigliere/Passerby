fb4197ac12fe1ded376e920c64b7dc21d10a004b
anonymoussc <50c5ac69@opayq.com>
2019-01-02 10:56:44 +0700

    Modified api \routes

--- Routes/api.php
+++ Routes/api.php
@@ -17,21 +17,17 @@ use Illuminate\Http\Request;
 //    return $request->user();
 //});
 
-if (config('password.routes.api.active')) {
-//    Route::group(['prefix' => 'api'], function() {
-    Route::group(['prefix' => 'v1'], function () {
-//            Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function() {
-//                Route::get('/intest', 'LoginController@inTests');
-//                Route::get('/outest', 'LoginController@authTests');
-//            });
-    });
-
-    Route::group(['middleware' => 'api', 'prefix' => 'login', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
-        Route::post('/', 'LoginController@login');
-        Route::post('/refresh', 'LoginController@refresh');
-    });
-    Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
-        Route::post('/logout', 'LoginController@logout');
-    });
+Route::group(['prefix' => 'v1'], function () {
+//    Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
+//        Route::get('/intest', 'LoginController@inTests');
+//        Route::get('/outest', 'LoginController@authTests');
 //    });
-}
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'login', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
+    Route::post('/', 'LoginController@login');
+    Route::post('/refresh', 'LoginController@refresh');
+});
+Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
+    Route::post('/logout', 'LoginController@logout');
+});

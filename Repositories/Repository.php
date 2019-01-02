ceec3e01068b1f255b0f8ae72e14a8fb538e2397
anonymoussc <50c5ac69@opayq.com>
2019-01-02 10:54:25 +0700

    Modified Repository App\Components\Passerby\Repositories

--- Repositories/Repository.php
+++ Repositories/Repository.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * DatabaseRepository.php
+ * Repository.php
  * Created by @anonymoussc on 10/22/2017 1:30 AM.
  */
 
 namespace App\Components\Passerby\Repositories;
 
 use App\Components\Signature\Repositories\SignatureRepository as BaseRepository;
+use App\Components\Signal\Shared\Signal;
+use App\Components\Signal\Shared\ErrorLog;
 
 abstract class Repository extends BaseRepository
 {
-
+    use Signal, ErrorLog;
 }

255055721ead1a6ce83b54c177a25a153c807b43
anonymoussc <50c5ac69@opayq.com>
2019-01-04 09:14:54 +0700

    New Service App\Components\Passerby\Services

---
+++ Services/Service.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Service.php
+ * Created by @anonymoussc on 01/03/2019 5:55 PM.
+ */
+
+namespace App\Components\Passerby\Services;
+
+use App\Components\Signal\Shared\Signal;
+use App\Components\Signal\Shared\ErrorLog;
+
+
+class Service
+{
+    use Signal, ErrorLog;
+
+}

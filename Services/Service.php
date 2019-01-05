51b73001d575c44e37885ea467a101f206d42fe9
anonymoussc <50c5ac69@opayq.com>
2019-01-06 04:33:11 +0700

    Modified Service App\Components\Passerby\Services

--- Services/Service.php
+++ Services/Service.php
@@ -9,7 +9,10 @@ namespace App\Components\Passerby\Services;
 use App\Components\Signal\Shared\Signal;
 use App\Components\Signal\Shared\ErrorLog;
 
-
+/**
+ * Class Service
+ * @package App\Components\Passerby\Services
+ */
 class Service
 {
     use Signal, ErrorLog;

5092b8803993e92ff7a5040217f8c9efdc2cc595
anonymoussc <50c5ac69@opayq.com>
2019-01-06 04:30:28 +0700

    Modified User App\Components\Passerby\Entities

--- Entities/User.php
+++ Entities/User.php
@@ -4,7 +4,14 @@ namespace App\Components\Passerby\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class User
+ * @package App\Components\Passerby\Entities
+ */
 class User extends Model
 {
+    /**
+     * @var array
+     */
     protected $fillable = [];
 }

15d239b6587ff9a42c4619bc1a6e5088565ae203
anonymoussc <50c5ac69@opayq.com>
2019-05-03 02:59:13 +0700

    perf(services): modified ./Services/Service

--- Services/Service.php
+++ Services/Service.php
@@ -1,20 +1,26 @@
 <?php
 /**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/3/19 2:59 AM
+ */
+
+/**
  * Service.php
  * Created by @anonymoussc on 01/03/2019 5:55 PM.
  */
 
 namespace App\Components\Passerby\Services;
 
-use App\Components\Signal\Shared\Signal;
-use App\Components\Signal\Shared\ErrorLog;
+use App\Components\Signal\Shared\{
+    ErrorLog, Signal
+};
+use App\Components\Signature\Services\SignatureService as BaseService;
 
 /**
  * Class Service
  * @package App\Components\Passerby\Services
  */
-class Service
+class Service extends BaseService
 {
     use Signal, ErrorLog;
-
 }

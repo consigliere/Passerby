2cebd1c85d08089e825646c4425d03e616f6ecd2
anonymoussc <50c5ac69@opayq.com>
2019-05-15 09:22:08 +0700

    refactor(repositories): Modified ./Repositories/AuthRepositoryInterface

--- Repositories/AuthRepositoryInterface.php
+++ Repositories/AuthRepositoryInterface.php
@@ -1,7 +1,17 @@
 <?php
 /**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/15/19 9:21 AM
+ */
+
+/**
  * LoginRepositoryInterface.php
  * Created by @anonymoussc on 11/10/2017 10:33 AM.
+ */
+
+/**
+ * AuthRepositoryInterface.php
+ * @renamed by @anonymoussc on 05/15/2019 9:20 AM.
  */
 
 namespace App\Components\Passerby\Repositories;
@@ -9,10 +19,10 @@ namespace App\Components\Passerby\Repositories;
 use App\Components\Passerby\Entities\User;
 
 /**
- * Interface LoginRepositoryInterface
+ * Interface AuthRepositoryInterface
  * @package App\Components\Passerby\Repositories
  */
-interface LoginRepositoryInterface
+interface AuthRepositoryInterface
 {
     /**
      * @param array $data

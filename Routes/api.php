f0cebe2ade602890de838c6089fa4bb9210254b2
anonymoussc <50c5ac69@opayq.com>
2019-05-15 09:58:54 +0700

    refactor(routes): Modified ./Routes/api

--- Routes/api.php
+++ Routes/api.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/15/19 9:54 AM
+ * Last modified 5/15/19 9:56 AM
  */
 
 /*
@@ -14,15 +14,11 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware' => 'api',
-              'prefix'     => 'authenticate',
-              'namespace'  => '\App\Components\Passerby\Http\Controllers'], function() {
-    Route::post('/', 'AuthController@login');
-});
-Route::group(['middleware' => 'api', 'prefix' => 'login', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
-    Route::post('/', 'AuthController@login');
+Route::group(['middleware' => 'api', 'prefix' => '', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function() {
+    Route::post('/authenticate', 'AuthController@login');
+    Route::post('/login', 'AuthController@login');
     Route::post('/refresh', 'AuthController@refresh');
 });
-Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
+Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function() {
     Route::post('/logout', 'AuthController@logout');
 });

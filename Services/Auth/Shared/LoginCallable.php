96e231f272623ac8aada75247b28e553ac8ef4a9
anonymoussc <50c5ac69@opayq.com>
2019-05-15 09:00:26 +0700

    refactor(services): Modified ./Services/Auth/Shared/LoginCallable

--- Services/Auth/Shared/LoginCallable.php
+++ Services/Auth/Shared/LoginCallable.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 2/28/19 2:25 AM
+ * Last modified 5/15/19 8:59 AM
  */
 
 /**
@@ -9,11 +9,11 @@
  * Created by @anonymoussc on 01/10/2019 6:11 AM.
  */
 
-namespace App\Components\Passerby\Services\Login\Shared;
+namespace App\Components\Passerby\Services\Auth\Shared;
 
 /**
  * Trait LoginCallable
- * @package App\Components\Passerby\Services\Login\Shared
+ * @package App\Components\Passerby\Services\Auth\Shared
  */
 trait LoginCallable
 {

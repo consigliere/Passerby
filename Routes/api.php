81d902cb5d63cee1fb5d786eff41916a18a1af00
anonymoussc <50c5ac69@opayq.com>
2019-05-15 09:54:58 +0700

    refactor(routes): Modified ./Routes/api

--- Routes/api.php
+++ Routes/api.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/15/19 8:01 AM
+ * Last modified 5/15/19 9:54 AM
  */
 
 /*
@@ -14,7 +14,11 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::group(['middleware' => 'api',
+              'prefix'     => 'authenticate',
+              'namespace'  => '\App\Components\Passerby\Http\Controllers'], function() {
+    Route::post('/', 'AuthController@login');
+});
 Route::group(['middleware' => 'api', 'prefix' => 'login', 'namespace' => '\App\Components\Passerby\Http\Controllers'], function () {
     Route::post('/', 'AuthController@login');
     Route::post('/refresh', 'AuthController@refresh');

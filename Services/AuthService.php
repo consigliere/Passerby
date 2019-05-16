00d26f5129dff978c9856b6535af2771d57f779f
anonymoussc <50c5ac69@opayq.com>
2019-05-16 08:24:53 +0700

    refactor(services): modified ./Services/AuthService

--- Services/AuthService.php
+++ Services/AuthService.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/15/19 9:29 AM
+ * Last modified 5/16/19 8:19 AM
  */
 
 /**
@@ -72,20 +72,21 @@ class AuthService extends Service
     }
 
     /**
-     * @param $username
-     * @param $password
+     * @param array $data
+     * @param array $option
+     * @param array $param
      *
      * @return array
      */
-    public function attemptLogin($username, $password): array
+    public function attemptLogin(array $data = [], array $option = [], array $param = []): array
     {
-        $user = $this->authRepository->getUserByUsernameOrEmail($username)->first();
+        $user = $this->authRepository->getUserByUsernameOrEmail(data_get($data, 'form.username'))->first();
 
         if ($user !== null) {
-            $proxy = $this->proxy(new Proxy, 'password', ['username' => $username, 'password' => $password]);
+            $proxy = $this->proxy(new Proxy, 'password', $data['form']);
 
             if (Config::get('password.log.info.login.active')) {
-                Event::dispatch('login.message', [['username' => $user->name, 'userid' => $user->id]]);
+                Event::dispatch('login.message', [['user' => $user,]]);
             }
 
             return $proxy;
@@ -95,19 +96,19 @@ class AuthService extends Service
     }
 
     /**
+     * @param array $data
+     * @param array $option
      * @param array $param
      *
      * @return array
      */
-    public function attemptRefresh(array $param = []): array
+    public function attemptRefresh(array $data = [], array $option = [], array $param = []): array
     {
-        $token = (
-            (!Config::get('password.refreshToken.cookie.httpOnly.')) &&
-            (true === isset($param['refresh_token'])) &&
-            ($param['refresh_token'] !== '')
-        )
-            ? $param['refresh_token']
-            : $this->request->cookie(self::REFRESH_TOKEN);
+        if (isset($data['refresh_token']) && !empty($data['refresh_token']) && ($data['refresh_token'] !== null) && (!$option['refresh.cookie.httpOnly'])) {
+            $token = $data['refresh_token'];
+        } else {
+            $token = $this->request->cookie(self::REFRESH_TOKEN);
+        }
 
         $proxy = $this->proxy(new Proxy, 'refresh_token', ['refresh_token' => $token]);
 
@@ -119,9 +120,11 @@ class AuthService extends Service
     }
 
     /**
-     * @return void
+     * @param array $data
+     * @param array $option
+     * @param array $param
      */
-    public function logout(): void
+    public function logout(array $data = [], array $option = [], array $param = []): void
     {
         $accessToken  = $this->auth->user()->token();
         $usertoken    = $accessToken;
@@ -132,7 +135,7 @@ class AuthService extends Service
         $this->cookie->queue($this->cookie->forget(self::REFRESH_TOKEN));
 
         if (Config::get('password.log.info.logout.active')) {
-            Event::dispatch('login.logout', [['userid' => $usertoken->user_id, 'usertokenid' => $usertoken->id]]);
+            Event::dispatch('login.logout', [['useruuid' => $param['auth.user.uuid'], 'username' => $param['auth.user.username'], 'usertokenid' => $usertoken->id]]);
         }
     }
 }

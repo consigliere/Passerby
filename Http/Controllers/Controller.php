0bce37095fdcfb96535c756976156b24b429ee66
anonymoussc <50c5ac69@opayq.com>
2019-07-19 23:35:11 +0700

    refactor(Controllers): modified ./Http/Controllers/Controller
    remove dead unused code

--- Http/Controllers/Controller.php
+++ Http/Controllers/Controller.php
@@ -6,7 +6,7 @@
 
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 7/8/19 6:00 AM
+ * Last modified 7/19/19 11:34 PM
  */
 
 namespace App\Components\Passerby\Http\Controllers;
@@ -14,12 +14,9 @@ namespace App\Components\Passerby\Http\Controllers;
 use App\Components\Signal\Shared\ErrorLog;
 use App\Components\Signal\Shared\Signal;
 use App\Components\Signature\Http\Controllers\SignatureController as BaseController;
-use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\JsonResponse;
-use JsonSerializable;
 
 /**
  * Class Controller
@@ -33,20 +30,4 @@ abstract class Controller extends BaseController
      * @var string
      */
     protected $euuid;
-
-    /**
-     * @param       $data
-     * @param int   $statusCode
-     * @param array $headers
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function response($data, $statusCode = 200, array $headers = ['Content-Type' => 'application/vnd.api+json',]): JsonResponse
-    {
-        if ($data instanceof Arrayable && !$data instanceof JsonSerializable) {
-            $data = $data->toArray();
-        }
-
-        return new JsonResponse($data, $statusCode, $headers);
-    }
 }

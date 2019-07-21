975df9d89e234fde542a15f5ef7a97a75e11cc39
anonymoussc <50c5ac69@opayq.com>
2019-07-21 08:18:43 +0700

    refactor(Tests): modified ./Tests/PasswordApiTestCase

--- Tests/PasswordApiTestCase.php
+++ Tests/PasswordApiTestCase.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * PasserbyApiTestCase.php
+ * PasswordApiTestCase.php
  * Created by @anonymoussc on 07/21/2019 7:46 AM.
  */
 
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 7/21/19 7:48 AM
+ * Last modified 7/21/19 8:10 AM
  */
 
 namespace Tests;
@@ -16,17 +16,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\Passport\Passport;
 
 /**
- * Class PasserbyApiTestCase
+ * Class PasswordApiTestCase
  * @package Tests
  */
-abstract class PasserbyApiTestCase extends BaseTestCase
+abstract class PasswordApiTestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseTransactions;
 
     public function init(): void
     {
         Passport::actingAs(
-            factory(\Api\User\Entities\User::class)->create(['role_id' => 2, 'uuid' => randomUuid(), 'username' => 'test' . mt_rand()])
+            factory(\Api\User\Entities\User::class)->create(['uuid' => randomUuid(), 'username' => 'test' . mt_rand()])
         );
     }
 }
